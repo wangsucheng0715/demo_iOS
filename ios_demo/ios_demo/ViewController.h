9991c1f259109d29e0e20fe07190e0efc4d33392
wsc <wsc@wscdeMacBook-Air.local>
2015-09-11 14:18:43 +0800

    提交文件

---
+++ ios_demo/ios_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios_demo
+//
+//  Created by wsc on 15/9/11.
+//  Copyright (c) 2015年 wsc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios_demo/ios_demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ios_demo
+//
+//  Created by wsc on 15/9/11.
+//  Copyright (c) 2015年 wsc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
